523c50bf08782089da6007a19c24a496b9e1402a
vsotirasqmplus <v.sotiras@qmul.ac.uk>
2016-11-22 15:40:44 +0000

    merge

--- version.php
+++ version.php
@@ -29,15 +29,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin = new \stdClass();
-<<<<<<< HEAD
 $plugin->version    = 2016111509;
-=======
-<<<<<<< HEAD
-$plugin->version    = 2016111509;
-=======
-$plugin->version    = 2016111509;
->>>>>>> 9093bd99a7d61ef96a58f74a26521055308f5767
->>>>>>> 807315c3944ab2db8b61f8be890b5254815a2ca2
 $plugin->requires   = 2015111604; // Moodle 3.0.4 is required.
 $plugin->component  = 'block_glsubs'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity   = MATURITY_BETA;
