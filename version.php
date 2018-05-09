d4e1783adee34eede3aeabf4d9127cb92a24a69e
Vasileios Sotiras <v.sotiras@qmul.ac.uk>
2018-05-09 13:01:49 +0100

    Conversion to PHP 7 Exception Handling

--- version.php
+++ version.php
@@ -40,8 +40,8 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin = new \stdClass();
-$plugin->version    = 2017072000;
-$plugin->requires   = 2015111604; // Moodle 3.0.4 is required.
+$plugin->version    = 2018050900;
+$plugin->requires   = 2017111302; // Moodle 3.4.2 is required.
 $plugin->component  = 'block_glsubs'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->cron = 0 ; // not using the old method, but the tasks new method
